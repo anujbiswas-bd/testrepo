9d0ebfdff8f02ab987bf5a8e4d425f9c26a6e260
anujbiswas-bd <anujbiswas.bd@gmail.com>
2019-04-10 10:49:35 +0600

    HelloWorld.java modified

--- HelloWorld.java
+++ HelloWorld.java
@@ -35,6 +35,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
-	System.out.println("Hi!!!");
+	System.out.println("Hi all!!!");
   }
 }
